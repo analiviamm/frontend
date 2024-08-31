5e39470e551de77223701962a9ea96975ae5ef7e
analiviamm <livialivia2307@gmail.com>
2024-08-31 17:02:53 -0300

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <MainMenu></MainMenu>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import MainMenu from "@/components/MainMenu.vue";
+
+export default {
+  name: 'App',
+
+  components: {
+    MainMenu,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
---
+++ src/components/MainMenu.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/logoime.png')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Apoio à Decisão IDQBRN
+        </h1>
+
+
+        <p class="subheading font-weight-regular">
+          Esse aplicativo tem como objetivo fornecer informações para
+          melhor tomada de decisão frente a uma possível ameaça radiológica.
+        </p>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <v-btn>Imputar Dados</v-btn>
+
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'MainMenu',
+
+  data: () => ({
+
+  }),
+}
+</script>
