2bef62bb3cc48132ef94f9e45feeddb355242dfd
analiviamm <livialivia2307@gmail.com>
2024-09-02 21:23:42 -0300

    Fix sidebar style

--- src/components/SidebarMenu.vue
+++ src/components/SidebarMenu.vue
@@ -2,16 +2,16 @@
   <div>
     <v-row v-if="!drawer" class="btn-visibility">
       <v-col cols="12" xs="12" sm="12" md="12" lg="12" xl="12" style="display: flex; justify-content: space-between">
-        <v-btn @click.stop="toggleDrawer" elevation="0" floating>
-          <v-icon background-color="black">mdi mdi-bookmark</v-icon>
+        <v-btn @click.stop="toggleDrawer" elevation="0" floating class="menu-button">
+          <v-icon>mdi mdi-bookmark</v-icon>
         </v-btn>
       </v-col>
     </v-row>
-    <v-navigation-drawer fixed elevation="0" floating :width="drawerWidth" v-model="drawer">
+    <v-navigation-drawer fixed elevation="0" floating :width="drawerWidth" v-model="drawer" class="sidebar">
       <div class="sidebar-menu">
         <v-img
           :src="require('../assets/logoime.png')"
-          style="width: 280px; height: 200px; margin-top: -300px; margin-bottom: -60px;"
+          class="logo"
         />
         <router-link to="/" v-slot="{ navigate }" style="text-decoration: none">
           <v-btn role="link" @click="navigate" class="list-link">
@@ -29,16 +29,15 @@
           </v-btn>
         </router-link>
         <v-btn icon @click="toggleDrawer" class="close-button">
-        <v-icon>mdi-close</v-icon>
-      </v-btn>
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
       </div>
     </v-navigation-drawer>
   </div>
 </template>
 
 <script>
-
-import {ref, computed} from "vue";
+import { ref, computed } from "vue";
 
 export default {
   name: 'SidebarMenu',
@@ -48,33 +47,27 @@ export default {
   emits: ['close_drawer'],
   setup(props, { emit }) {
 
-    // components variables
-    const drawer = ref(window.innerWidth >= 900)
+    const drawer = ref(window.innerWidth >= 900);
 
-    //updates on route change
     const drawerWidth = computed(() => {
-      return window.innerWidth < 500 ? 300 : 360
-    })
+      return window.innerWidth < 500 ? 300 : 360;
+    });
 
-    // utils functions
     function toggleDrawer() {
-      drawer.value = !drawer.value
-      emit("close_drawer", {'happened': drawer.value})
-      console.log("emitindo")
+      drawer.value = !drawer.value;
+      emit("close_drawer", {'happened': drawer.value});
     }
-
 
     return {
       drawer,
       drawerWidth,
       toggleDrawer,
-    }
+    };
   }
 }
 </script>
 
-<style>
-
+<style scoped>
 .sidebar-menu {
   display: flex;
   justify-content: center;
@@ -85,10 +78,21 @@ export default {
   width: 100%;
 }
 
-.sidebar-menu>* {
+.sidebar-menu > * {
   max-width: 260px;
 }
 
+.sidebar {
+  background-color: #2596be; /* Azul consistente com o tema */
+  color: white;
+}
+
+.logo {
+  width: 280px;
+  height: 200px;
+  margin-top:-300px;
+  margin-bottom: -60px;
+}
 
 .btn-visibility {
   margin-bottom: 10px;
@@ -98,10 +102,19 @@ export default {
   width: 100%;
 }
 
+.menu-button {
+  background-color: #2596be; /* Cor de fundo azul para o botão */
+  color: white;
+}
+
 .close-button {
   position: absolute;
   top: 16px;
   right: 16px;
+  color: #2596be;
 }
 
+.list-link {
+  color: #2596be; /* Garantir que os links estejam visíveis sobre o fundo azul */
+}
 </style>
